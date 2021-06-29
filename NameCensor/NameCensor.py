0e75b8e8fb1e821dc1a43cf3d34c0abdfb892c11
Nadie <hackerjef@users.noreply.github.com>
2021-06-29 12:26:06 -0400

    Maybe this is the correct way?

--- NameCensor/NameCensor.py
+++ NameCensor/NameCensor.py
@@ -9,18 +9,38 @@ class NameCensorPlugin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
-        self.enabled = True
-        self.whitelist = set()
-        asyncio.create_task(self._set_config())
 
-    async def _set_config(self):
-        config = await self.db.find_one({"_id": "NicknameCensor"})
-        print(config)
-        print(type(config))
+        self.enabled = True
+        self.redlist_nicknames: list = list()
+        self.redlist_usernames: list = list()
+        self.greenlist_users: list = list()
+        asyncio.create_task(self._set_options())
+
+    async def _update_config(self):
+        await self.db.find_one_and_update(
+            {"_id": "config"},
+            {
+                "$set": {
+                    "enabled": self.enabled,
+                    "greenlist_users": self.greenlist_users,
+                    "redlist_nicknames": self.redlist_nicknames,
+                    "redlist_usernames": self.redlist_usernames
+                }
+            },
+            upsert=True,
+        )
+
+    async def _set_options(self):
+        config = await self.db.find_one({"_id": "config"})
+
+        if config is None:
+            await self._update_config()
+            return
+
         self.enabled = config.get("enabled", True)
-        self.redlist_nicknames = set(config.get("redlist_nicknames", []))
-        self.redlist_usernames = set(config.get("redlist_usernames", []))
-        self.greenlist_users = set(config.get("user_greenlist", []))
+        self.redlist_nicknames = config.get("redlist_nicknames", [])
+        self.redlist_usernames = config.get("redlist_usernames", [])
+        self.greenlist_users = config.get("user_greenlist", [])
 
     @commands.group(invoke_without_command=True)
     @commands.is_owner()
@@ -30,11 +50,7 @@ class NameCensorPlugin(commands.Cog):
         Usage: `namecensor enable` / `namecensor disable`
         """
         self.enabled = mode
-
-        await self.coll.update_one(
-            {"_id": "config"}, {"$set": {"enabled": self.enabled}}, upsert=True
-        )
-
+        await self._update_config()
         await ctx.send(("Enabled" if mode else "Disabled") + " the namecensor filter.")
 
 
