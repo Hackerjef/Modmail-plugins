e447f9ccf553081d8248f345b8a707e7206f653b
Nadie <hackerjef@users.noreply.github.com>
2021-11-03 00:57:31 -0400

    allow roles to be added to whitelist

--- logviewer2companion/logviewer2companion.py
+++ logviewer2companion/logviewer2companion.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Union
 
 import discord
 from discord.ext import commands
@@ -20,6 +21,7 @@ class logviewer2companion(commands.Cog):
         self.enabled = True
         self.allow_evidence_share = False
         self.allowed_users: list = list()
+        self.allowed_roles: dict = dict()
         asyncio.create_task(self._set_options())
 
     async def _update_config(self):
@@ -30,6 +32,7 @@ class logviewer2companion(commands.Cog):
                     "enabled": self.enabled,
                     "allow_evidence_share": self.allow_evidence_share,
                     "allowed_users": self.allowed_users,
+                    "allowed_roles": self.allowed_roles,
                 }
             },
             upsert=True,
@@ -45,6 +48,24 @@ class logviewer2companion(commands.Cog):
         self.enabled = config.get("enabled", True)
         self.allow_evidence_share = config.get("allow_evidence_share", False)
         self.allowed_users = config.get("allowed_users", [])
+        self.allowed_roles = config.get("allowed_roles", [])
+
+    async def update_roles(self):
+        g = self.bot.modmail_guild
+        if not g:
+            return
+
+        for role in self.allowed_roles:
+            grole = g.get_role(role)
+            if grole:
+                members = []
+                for member in grole.members:
+                    members.append(member.id)
+                self.allowed_roles = members
+            else:
+                del self.allowed_roles[role]
+
+        await self._update_config()
 
     @commands.group(name="l2c", invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.OWNER)
@@ -88,7 +109,9 @@ class logviewer2companion(commands.Cog):
         Usage: `eloglink`
         """
         if not self.allow_evidence_share:
-            return await ctx.send("Cannot get evidence loglink, Please enabled it (<p>l2c evidenceshare)")
+            embed = discord.Embed(color=self.bot.error_color)
+            embed.description = "Cannot get evidence loglink, Please enabled it (<p>l2c evidenceshare)"
+            return await ctx.send(embed=embed)
 
         log = await self.bot.api.get_log(ctx.channel.id)
         embed = discord.Embed(color=self.bot.main_color)
@@ -129,38 +152,64 @@ class logviewer2companion(commands.Cog):
         embed = discord.Embed(color=self.bot.main_color)
         embed.title = "Allowed users:"
         description = []
+        users = []
         for uid in self.allowed_users:
             user = self.bot.get_user(uid)
+            users.append(uid)
             description.append(f"{str(user)} - `{user.id}`")
+
+        for rid in self.allowed_roles:
+            for uid in self.allowed_roles[rid]:
+                if uid not in users:
+                    user = self.bot.get_user(uid)
+                    users.append(uid)
+                    description.append(f"{str(user)} - `{user.id}` - role: {rid}")
         embed.description = "\n".join(description)
+
         return await ctx.send(embed=embed)
 
-    @oauth2.command(name="user")
+    @oauth2.command(name="whitelist")
     @checks.has_permissions(PermissionLevel.OWNER)
-    async def oauth_user(self, ctx, mode: str, user: utils.User):
+    async def oauth_user(self, ctx, mode: str, target: Union[discord.Role, utils.User]):
         """Gives permissions on who can/cannot see oauth protected logs and checks status on users
 
-        Usage: `oauth2 (add, remove, info) <userID>`
+        Usage: `oauth2 whitelist (add, remove, info) <userID>`
         """
 
         embed = discord.Embed(color=self.bot.main_color)
 
+        # yes was taken from core/utilitys but it works :)
+        if not hasattr(target, "mention"):
+            target = self.bot.get_user(target.id) or self.bot.modmail_guild.get_role(target.id)
+
         if mode == "add" or mode == "set":
-            self.allowed_users.append(user.id)
-            await self._update_config()
-            embed.description = f"User {str(user)} ({user.id}) has been added"
-            return await ctx.send(embed=embed)
+            if isinstance(target, discord.Role):
+                self.allowed_roles[target.id] = []
+            else:
+                self.allowed_users.append(target.id)
+
         elif mode == "remove" or mode == "rmv":
-            self.allowed_users.remove(user.id)
-            await self._update_config()
-            embed.description = f"User {str(user)} ({user.id}) has been removed"
-            return await ctx.send(embed=embed)
+            if isinstance(target, discord.Role):
+                del self.allowed_roles[target.id]
+            else:
+                self.allowed_users.remove(target.id)
+
         elif mode == "status" or mode == "info":
-            verbage = "has" if user.id in self.allowed_users else "doesn't have"
-            embed.description = f"User {str(user)} ({user.id}) {verbage} access to oauth logs"
-            return await ctx.send(embed=embed)
+            if isinstance(target, discord.Role):
+                verbage = "has" if target.id in self.allowed_roles else "doesn't have"
+            else:
+                verbage = "has" if target.id in self.allowed_users else "doesn't have"
+
+            embed.description = f"{target.mention} ({target.id}) {verbage} access to oauth logs"
         else:
             raise commands.BadArgument("Invalid usage, allowed options `add, remove status`")
+
+        if mode in ["add", "set", "remove", "rmv"]:
+            await self._update_config()
+            asyncio.create_task(self.update_roles())
+            embed.description = f"{target.mention} ({target.id}) has been edited"
+
+        return await ctx.send(embed=embed)
 
 
 def setup(bot):
