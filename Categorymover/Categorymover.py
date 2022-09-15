440b9d7fe0ca1e83e29f3e5f5dce956fd91bfdbf
Nadie <hackerjef@users.noreply.github.com>
2022-09-15 12:59:07 -0400

    Debug + Better msgs

--- Categorymover/Categorymover.py
+++ Categorymover/Categorymover.py
@@ -48,8 +48,8 @@ class ReactionMenu(object):
         await self.reaction_addr
         if moved_to:
             asyncio.create_task(self._clear_reactions())
-            await self.menu.edit(embed=discord.Embed(color=self.cog.bot.main_color, description=f"✅ Moved to `{self.cog.categories.get(moved_to, 'Unknown')}`"))
-            await self.thread.channel.send(embed=discord.Embed(description=f"Moved to {str(moved_to)}", color=self.cog.bot.main_color))
+            await self.menu.edit(embed=discord.Embed(color=self.cog.bot.main_color, description=f"✅ Moved to `{self.cog.categories.get(str(moved_to.id), 'Unknown')}`"))
+            await self.thread.channel.send(embed=discord.Embed(description=f"Moved to <#{moved_to.id}>", color=self.cog.bot.main_color))
         else:
             await self.menu.delete()
         del self.cog.running_responses[self.thread.id]
@@ -62,7 +62,7 @@ class ReactionMenu(object):
         category = discord.utils.get(self.cog.bot.modmail_guild.categories, id=int(self.options[payload.emoji.name]))
         if category:
             await self.thread.channel.move(category=category, end=True, sync_permissions=True, reason="Thread was moved by Reaction menu within modmail")
-        await self.disband(moved_to=int(self.options[payload.emoji.name]))
+        await self.disband(moved_to=category)
 
     async def _add_reactions(self):
         try:
@@ -71,6 +71,13 @@ class ReactionMenu(object):
         except asyncio.CancelledError:
             pass
 
+    async def _clear_reactions(self):
+        for reaction in self.menu.reactions:
+            print(reaction)
+            if reaction.me:
+                print("me")
+                await reaction.remove(self.bot.user.id)
+
     def _gen_embed(self):
         embed = discord.Embed(color=self.cog.bot.main_color)
         rows = [self.cog.menu_description + "\n"]
@@ -78,11 +85,6 @@ class ReactionMenu(object):
             rows.append(f"{emoji} - {self.cog.categories.get(category, 'Unknown')}")
         embed.description = "\n".join(rows)
         return embed
-
-    async def _clear_reactions(self):
-        for reaction in self.menu.reactions:
-            if reaction.me:
-                await reaction.remove(self.bot.user.id)
 
 
 class Categorymoverplugin(commands.Cog):
