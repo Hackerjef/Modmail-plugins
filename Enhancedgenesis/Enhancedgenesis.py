df72e20fa5b33186769b307da828232a691bcc29
Nadie <hackerjef@users.noreply.github.com>
2022-09-20 13:21:37 -0400

    Reflect discord.py update

--- Enhancedgenesis/Enhancedgenesis.py
+++ Enhancedgenesis/Enhancedgenesis.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+from core.thread import Thread
 from core.utils import days
 from discord.ext import commands
 from discord.utils import snowflake_time
@@ -12,12 +14,15 @@ class Enhancedgenesisplugin(commands.Cog):
 
     # Rewrite genesis message to be formated better
     @commands.Cog.listener()
-    async def on_thread_ready(self, thread, creator, category, initial_message):
+    async def on_thread_ready(self, thread: Thread, creator, category, initial_message):
         # Grab older embed data from msg (only change description)
         try:
-            embed = thread.genesis_message.embeds[0]
+            genesis_message = await thread.get_genesis_message()
+            if not genesis_message:
+                return
+            embed = genesis_message.embeds[0]
         except Exception:
-            raise Exception("Message doesn't have an embed")
+            raise Exception("Message doesn't have an embed/genesis doesn't exist")
 
         member = self.bot.guild.get_member(thread._recipient.id)
         time = datetime.utcnow()
@@ -43,7 +48,7 @@ class Enhancedgenesisplugin(commands.Cog):
             embed_description.append(f"**{thread_word} Created:** {str(log_count)}")
 
         embed.description = "\n".join(embed_description)
-        await thread.genesis_message.edit(content=thread.genesis_message.content, embed=embed)
+        await genesis_message.edit(content=genesis_message.content, embed=embed)
 
 
 async def setup(bot):
